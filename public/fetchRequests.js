01bfc22b3a42aa4c5eccb04e898c732701995a31
Sem Bauke <semboot699@gmail.com>
2021-07-22 22:08:31 +0200

    display front and back of the card

--- public/fetchRequests.js
+++ public/fetchRequests.js
@@ -20,11 +20,28 @@ function updateDisplay(card){
     console.log(card);
     document.getElementById("question").innerHTML = card["front"];
 }
+
+let frontBackObj = [];
+
+function switchFrontAndBack(){
+    let display = document.getElementById("question");
+    console.log(frontBackObj[0]["front"]);
+    let frontOrBack = document.getElementById("front-back");
+    if(display.innerHTML == frontBackObj[0]["front"]){
+        display.innerHTML = frontBackObj[0]["back"];
+        frontOrBack.innerHTML = "Back of the card";
+    } else {
+        display.innerHTML = frontBackObj[0]["front"];
+        frontOrBack.innerHTML = "Front of the card";
+    }
+}
+
 function fetchCards(category) {
     fetch('/cards')
         .then(response => response.json())
         .then(json => {
             updateDisplay(json);
+            frontBackObj.push(json);
             if(!category) return;
             insertCategory(json, category);
         })
